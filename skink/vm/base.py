e4721ee04754ae4c17e45323686401b72ab21a70
Andrews Medina <andrewsmedina@gmail.com>
2012-10-02 23:53:57 -0300

    added stop method on base VmManager

--- skink/vm/base.py
+++ skink/vm/base.py
@@ -2,3 +2,6 @@ class VmManager(object):
 
     def start(self):
         pass
+
+    def stop(self):
+        pass
--- tests/vm/test_vm.py
+++ tests/vm/test_vm.py
@@ -5,6 +5,11 @@ from skink.vm.base import VmManager
 
 class VmManagerTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.manager = VmManager()
+
     def test_vm_manager_start(self):
-        manager = VmManager()
-        manager.start()
+        self.manager.start()
+
+    def test_vm_managet_stop(self):
+        self.manager.stop()
