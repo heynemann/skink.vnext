60ce31cd46d9a5015894273157e7162fb4c0a39f
Andre Fonseca <aoqfonseca@gmail.com>
2012-10-14 13:48:39 -0300

    implement config data into config instance and putting all tests and var in english

--- skink/build/entities.py
+++ skink/build/entities.py
@@ -7,5 +7,8 @@ class Config(object):
     @classmethod
     def create_from_file(cls, file_path):
         stream = file(file_path, 'r')
-        yaml.load(stream)
-        return cls()
+        config_data = yaml.load(stream)
+        return cls(config_data)
+
+    def __init__(self, config_data):
+        self.__dict__ = config_data
--- tests/build/test_config.py
+++ tests/build/test_config.py
@@ -18,13 +18,17 @@ class ConfigTestCase(unittest.TestCase):
             patch('__builtin__.file'),
             patch('yaml.load')
         ) as (self.file_mock, self.yaml_load_mock):
-            self.config = Config.create_from_file("teste.yaml")
+            self.yaml_load_mock.return_value = {'tests': 'tests'}
+            self.config = Config.create_from_file("tests.yaml")
 
     def test_return_new_config_instance(self):
         assert isinstance(self.config, Config)
 
     def test_should_open_file_passed(self):
-        self.file_mock.assert_called_once_with('teste.yaml', 'r')
+        self.file_mock.assert_called_once_with('tests.yaml', 'r')
 
     def test_should_call_yaml_loader(self):
         self.yaml_load_mock.assert_called_once_with(ANY)
+
+    def test_copy_loaded_config_to_instance_dict(self):
+        self.assertEqual(self.config.__dict__, {'tests': 'tests'})
