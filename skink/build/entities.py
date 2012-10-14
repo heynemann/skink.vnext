dad12b3f0d13f6eabf900b8b29421ab5a88de5dd
Andre Fonseca <aoqfonseca@gmail.com>
2012-10-14 13:08:52 -0300

    better using mock in the ConfigTestCase

--- skink/build/entities.py
+++ skink/build/entities.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+import yaml
 
 
 class Config(object):
 
     @classmethod
     def create_from_file(cls, file_path):
-        open(file_path)
+        stream = file(file_path, 'r')
+        yaml.load(stream)
         return cls()
--- tests/build/test_config.py
+++ tests/build/test_config.py
@@ -5,29 +5,26 @@ try:
 except ImportError, e:
         import unittest
 
-from mock import patch, mock_open, ANY
+from mock import patch, ANY
+from contextlib import nested
 
 from skink.build.entities import Config
 
 
-@patch('__builtin__.open')
 class ConfigTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.config = Config()
+        with nested(
+            patch('__builtin__.file'),
+            patch('yaml.load')
+        ) as (self.file_mock, self.yaml_load_mock):
+            self.config = Config.create_from_file("teste.yaml")
 
-    def test_has_method_create_from_file(self, *args):
-        assert Config.create_from_file
+    def test_return_new_config_instance(self):
+        assert isinstance(self.config, Config)
 
-    def test_return_new_config_instance(self, *args):
-        some_file_path = "tmp/teste.yaml"
-        assert isinstance(Config.create_from_file(some_file_path), Config)
+    def test_should_open_file_passed(self):
+        self.file_mock.assert_called_once_with('teste.yaml', 'r')
 
-    def test_should_open_file_passed(self, open_mock):
-        Config.create_from_file('teste.yaml')
-        open_mock.assert_called_once_with('teste.yaml')
-
-    @patch('yaml.load')
-    def test_should_call_yaml_loader(self, open_mock, yaml_mock):
-        Config.create_from_file('teste.yaml')
-        yaml_mock.assert_called_once_with(ANY)
+    def test_should_call_yaml_loader(self):
+        self.yaml_load_mock.assert_called_once_with(ANY)
