59839359b898c460a93eb6d5480700ac7b26b2e7
Andre Fonseca <aoqfonseca@gmail.com>
2012-10-18 18:33:41 -0300

    putting initial logic for git monitoring in project

--- skink/models/__init__.py
+++ skink/models/__init__.py
@@ -19,6 +19,7 @@
 # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+import sh
 
 from redisco import models
 
@@ -32,13 +33,19 @@ class Project(models.Model):
 
 
     def clone(self):
-        pass
+        command =  '%s %s' %('clone', self.git_repo)
+        sh.git(command)
 
     def fetch(self):
         pass
 
     def check_update(self):
         return True
+
+    @property
+    def dir_repo(self):
+        return "/builds/%s" %self.name
+
 
     @property
     def next_build(self):
--- tests/models/test_project.py
+++ tests/models/test_project.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from mock import patch
+
 from skink.models import Project
 
 class ProjectTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.project = Project(git_repo = 'git@git://github.com/heynemman/skink.next')
+        self.project = Project(name=u"skink.next", git_repo = 'git@git://github.com/heynemman/skink.next')
 
     def test_has_clone_method(self):
         assert self.project.clone
@@ -16,4 +18,13 @@ class ProjectTestCase(unittest.TestCase):
 
     def test_has_check_update_method(self):
         assert self.project.check_update
+
+    def test_has_dir_repo(self):
+        assert self.project.dir_repo == '/builds/%s' % self.project.name
+
+    @patch('sh.git')
+    def test_call_git_clone(self, mock_git_clone):
+        self.project.clone()
+        expected_params = '%s %s' %('clone', self.project.git_repo)
+        mock_git_clone.assert_called_once_with(expected_params)
 
